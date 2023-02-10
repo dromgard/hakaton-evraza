e5488384f0df6fd586c0017552ef99b33dbdea2f
Sergei Korolev <dromgard@yandex.ru>
2023-02-10 12:56:37 +0300

    feat: Верстка Header

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__control">
+        <button className="button header__burger-menu" type="button" title="Открыть меню" aria-label="Открыть меню" />
+        <Link className="link logo" to="" aria-label="Перейти на 'Главную'" />
+        <p className="header__nav">Прогнозная аналитика эксгаустеров</p>
+      </div>
+      <div className="header__info">
+        <Link className="link header__handbook" to="" aria-label="Перейти на 'Справочник'" >Справочник</Link>
+        <button className="button header__notifications" type="button" title="Открыть уведомления" aria-label="Открыть уведомления">
+          <div className="header__notifications-new"></div>
+        </button>
+        <div className="header__list">
+          <p className="header__list-value">DN</p>
+          <button className="button header__list-button" type="button" title="Открыть список" aria-label="Открыть список" >
+            <p className="button-arrow">&gt;</p>
+          </button>
+        </div>
+      </div>
+
+    </header>
+  )
+}
+
+export default Header;
