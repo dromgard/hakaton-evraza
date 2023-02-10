953df75b93fa107d09b638a9c9b66111c7f89f0d
Sergei Korolev <dromgard@yandex.ru>
2023-02-10 16:21:02 +0300

    feat: Main. Начал верстку

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,6 +1,31 @@
 function Main() {
   return (
-    <h1>Это секция Main</h1>
+    <main className="main">
+      <div className="main__header">
+        <div className="main__header-logo"></div>
+        <h1 className="section-title">Главный экран</h1>
+      </div>
+      <ul className="main__info">
+        <li className="main__info-item">
+          <p className="info-text">Температура</p>
+        </li>
+        <li className="main__info-item">
+          <p className="info-text">Вибрация</p>
+        </li>
+        <li className="main__info-item">
+          <p className="info-text">Уровень масла</p>
+        </li>
+        <li className="main__info-item">
+          <div className="indicator-yellow"></div>
+          <p className="info-text">Предупреждение</p>
+        </li>
+        <li className="main__info-item">
+          <div className="indicator-red"></div>
+          <p className="info-text">Опасность</p>
+        </li>
+
+      </ul>
+    </main >
   )
 }
 
